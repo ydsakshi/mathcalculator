20f57a769a0a234951abaf643baa9f1acddad942
Sakshi Yadav <ydsakshi023@gmail.com>
2019-07-29 14:46:40 +0530

    trial

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,5 +10,6 @@ public static void main(String[] args) {
 	Calculator calculator=new Calculator();
 	System.out.println(calculator.add(10, 20));
 	System.out.println(calculator.mul(10, 20));
+	System.out.println(calculator.div(10, 20));
 }
 }
